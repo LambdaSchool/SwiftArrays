6d5820cae298f7b6e41d9a0d44a4fb4bc871053b
Spencer Curtis <spencercolecurtis@gmail.com>
2019-08-23 09:24:37 -0600

    Fix typo in playground's instructions

--- SwiftArrays.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
+++ SwiftArrays.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 /*:
  ## Arrays
- Create an array of 5 of your favorite TV shows. Then using subscription syntax print out the first element of your array. Copy and paste your array in each page of this assignment.
+ Create an array of 5 of your favorite TV shows. Then using subscript syntax print out the first element of your array. Copy and paste your array in each page of this assignment.
  */
 
 //: page 1 of 3 | [Next: Modifying Arrays](@next)
